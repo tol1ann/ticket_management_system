011c387775d9cc1084bfa6ed49e7cfd5e5eeee28
tol1an <tolian16995@gmail.com>
2023-12-11 18:43:49 +0300

    added auth/tests.py and tickets/tests.py

--- auth/tests.py
+++ auth/tests.py
@@ -1,3 +1,51 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+
+class AuthenticationAPITestCase(APITestCase):
+    def test_user_registration(self):
+        registration_data = {
+            'username': 'testuser',
+            'email': 'test5@test5.com',
+            'password': 'testpassword123',
+            'password_repeated': 'testpassword123',
+            'first_name': 'test',
+            'last_name': 'user'
+        }
+
+        response = self.client.post(reverse('auth_register'), data=registration_data)
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_user_login(self):
+        user = User.objects.create_user(username='testuser', password='testpassword123')
+
+        login_data = {
+            'username': 'testuser',
+            'password': 'testpassword123',
+        }
+
+        response = self.client.post(reverse('token_login'), data=login_data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
+
+        refresh = response.data['refresh']
+        refresh_token = RefreshToken(refresh)
+
+        response = self.client.post(reverse('token_refresh'), data={'refresh': refresh})
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+
+        access_token_after_refresh = response.data['access']
+        access_token = str(refresh_token.access_token)
+
+        self.assertNotEqual(access_token, access_token_after_refresh)
